15ce13f4de1be02fc030b50a4e9294b1b306c019
neto112 <cristianorneto@gmail.com>
2022-05-17 21:02:01 -0300

    First project Mobile

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { theme } from './src/theme';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Widget } from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      <Widget />
+      <StatusBar
+        style="light"
+        backgroundColor='transparent'
+        translucent
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Widget() {
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
